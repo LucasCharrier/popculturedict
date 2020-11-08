7113da40126402153b7038fa1895feaa33d6b670
lucharrier <lucharrier@gmail.com>
2020-11-08 15:24:49 +0100

    Removed middleware

--- app/Http/Controllers/Auth/MeController.php
+++ app/Http/Controllers/Auth/MeController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 
 class MeController extends Controller
 {
-    //
-    public function __construct()
-    {
-        $this->middleware(['auth:api']);
-    }
+    // //
+    // public function __construct()
+    // {
+    //     $this->middleware(['auth:api']);
+    // }
 
     public function __invoke(Request $request) {
         $user = $request->user();
