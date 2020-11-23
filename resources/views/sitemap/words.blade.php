d1df4fcaf57d44badf94f23af24acd77fa0b475c
lucharrier <lucharrier@gmail.com>
2020-11-23 12:04:48 +0100

    Fixed sitemap

--- resources/views/sitemap/words.blade.php
+++ resources/views/sitemap/words.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($words as $word)
         <url>
-            <loc><?php echo env('APP_FRONT_URL') ? env('APP_FRONT_URL') : 'https://lepopdictionnaire.herokuapp.com'; ?>/{{ implode("-", explode(" ", $word->name)) }}</loc>
+            <loc><?php echo env('APP_FRONT_URL') ? env('APP_FRONT_URL') : 'https://lepopdictionnaire.herokuapp.com'; ?>/{{ strtolower(implode("-", explode(" ", $word->name))) }}</loc>
             <lastmod>{{ $word->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.9</priority>
